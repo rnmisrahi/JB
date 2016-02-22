7f93a219c794fb81bb66fb3621b9d82f171e8a55
rnmisrahi <rnmisrahi@gmail.com>
2016-02-22 12:10:24 +0200

    Problem: After adding Employees, Members and Reservations... Migrations has a problem

---
+++ CarRental02/CarRental02/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace CarRental02.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CarRental02.Models.CarRentalContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "CarRental02.Models.CarRentalContext";
+        }
+
+        protected override void Seed(CarRental02.Models.CarRentalContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
